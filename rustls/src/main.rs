81f884c8ebb8c2c76d22b27b9853738ba46c0d57
moore <illyaipie@gmail.com>
2025-10-09 19:54:29 +0300

    rusls

---
+++ rustls/src/main.rs
@@ -0,0 +1,27 @@
+use std::fs;
+use std::path::Path;
+use std::error::Error;
+use std::process;
+
+fn main() {
+	if let Err(ref e) = run(Path::new(".")) {
+		println!("{}", e);
+		process::exit(1);
+	}
+}
+
+fn run(dir: &Path) -> Result<(), Box<dyn Error>> {
+	if dir.is_dir() {
+		for entry in fs::read_dir(dir)? {
+				let entry = entry?;
+				let file_name = entry
+						.file_name()
+						.into_string()
+						.or_else(|f| Err(format!("Invalid entry: {:?}", f)))?;
+				println!("{}", file_name);
+		}
+	}
+	Ok(())
+}
+
+
